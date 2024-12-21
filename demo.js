24c35127b1e1403f1794006b840258b4d2ffd85c
Sachin0072 <pathaniasachin74@gamil.com>
2024-12-21 19:49:47 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,25 @@
+// let a = 1 == true;
+// let b = 1 === true;
+
+// console.log(a);
+// console.log(b);
+
+let age = 25;
+
+if(age < 18)
+{
+    console.log("no entry");
+    
+}
+
+else if(age >= 18 && age <=25)
+{
+    console.log("kundi mt khdkao raja")
+}
+
+else 
+{
+
+    console.log("pile pile o more raja");
+    
+}
