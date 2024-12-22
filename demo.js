264799eca52b0f00efc037eedda2c252195562f8
Sachin0072 <pathaniasachin74@gamil.com>
2024-12-22 11:19:20 +0530

    first push

--- demo.js
+++ demo.js
@@ -1,25 +1,93 @@
-// let a = 1 == true;
-// let b = 1 === true;
+// // // // let a = 1 == true;
+// // // // let b = 1 === true;
 
-// console.log(a);
-// console.log(b);
+// // // // console.log(a);
+// // // // console.log(b);
 
-let age = 25;
+// // // let age = 25;
 
-if(age < 18)
-{
-    console.log("no entry");
+// // // if(age < 18)
+// // // {
+// // //     console.log("no entry");
     
-}
+// // // }
 
-else if(age >= 18 && age <=25)
-{
-    console.log("kundi mt khdkao raja")
-}
+// // // else if(age >= 18 && age <=25)
+// // // {
+// // //     console.log("kundi mt khdkao raja")
+// // // }
 
-else 
-{
+// // // else 
+// // // {
 
-    console.log("pile pile o more raja");
+// // //     console.log("pile pile o more raja");
     
-}
+// // // }
+
+
+
+// // // let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// // // for(i=0; i<= a.length; i++)
+// // // {
+// // //     console.log(i);
+    
+// // // }
+
+// // for(let i = 10; i>=1; i--)
+// // {
+   
+// //      console.log("*".repeat(i));
+   
+// // }
+
+// // let i = 0
+// // while(i<=10)
+// // {
+// //     console.log(i);
+// //     i++;
+    
+// // }
+
+// // let iii = 0;
+
+// // do
+// // {
+// //     console.log(iii);
+// //     iii++;
+    
+// // }while(iii<=10)
+
+// // let arr = [1, [3,[5,6],4],2]
+// // console.log(arr[1][1][0]);
+
+// // object 
+
+// let a = {
+//     name:"Sachin",
+//     age:26
+// }
+
+// console.log(typeof(a.age && a.name));
+
+
+// let i = 0;
+// space = ""
+// while(i>=10)
+// {
+//     console.log(`${space}*`.repeat(i));
+//     i++;
+    
+// }
+
+// let i =0;
+// do
+// {
+//     console.log("*".repeat(i));
+//     i++;
+    
+// }while(i<=10);
+
+
+let arr = [3,2,4,[1,2,2,[0,8,2,6],5,8,4],6,7,8]
+console.log(arr[3][3][1]);
